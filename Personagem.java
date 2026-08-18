495a157a02d91cbbf2584e09ec7c8108579bfc96
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2026-08-18 18:43:54 +0000

    Atributo comum todos os personagens

--- Personagem.java
+++ Personagem.java
@@ -1,33 +1,6 @@
-public class Personagem {
-    String nome;
-    String classe;
-    int nivel;
-    int pontosDeVida;
-    double poderBase;
+public abstract class Personagem {
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        this.classe = classe;
-        this.nivel = nivel;
-        this.pontosDeVida = pontosDeVida;
-        this.poderBase = poderBase;
-    
-        
-        
-
-    }
-
-    public Personagem() {
-        //TODO Auto-generated constructor stub
-    }
-
-    public void exibirStatus() {
-        System.out.println("Nome:" + nome);
-        System.out.println("Classe:" + classe);
-        System.out.println("Nível:" + nivel);
-        System.out.println("HP:" + pontosDeVida);
-        System.out.println("Poder Base:" + poderBase);
-        
-    }
+    // Atributo comum todos os personagens
+    protected string nome;
 }
 
