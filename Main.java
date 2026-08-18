d678097ee3ae0b5b9962aaf8a6348377bd34b43d
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2026-08-18 19:44:18 +0000

    objetos de diferentes subclasses

--- Main.java
+++ Main.java
@@ -2,3 +2,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Cria um vetor de Personagem contendo
+        // objetos de diferentes subclasses
+        Personagem[] grupo = {
+            new Guerreiro("Arthus"),
+            new Mago("Elenara")
+        };
+
