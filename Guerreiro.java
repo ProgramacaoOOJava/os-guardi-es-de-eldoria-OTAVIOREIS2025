9924d37d82b607fe6a40cb8fbebc8c8d6d91ad64
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2026-08-18 19:05:17 +0000

    // Subclasse Guerreiro que herda de Personagem

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,3 +1,9 @@
-public class Guerreiro {
+
+// Subclasse Guerreiro que herda de Personagem
+public class Guerreiro extends Personagem {
+ 
+// Atributo  próprio do guerreiro
+public Guerreiro(String nome) {
     
+}   
 }
